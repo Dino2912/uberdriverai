7aebecc5af943ff908439cd3fddd1a3628afee49
Odin Talgoe <coaly44@gmail.com>
2021-06-28 19:07:47 +0200

    Nothing

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -20,14 +20,16 @@ class _HomePageState extends State<HomePage> {
   bool carTurningRight = false;
 
   void startGame() {
-    gameStarted = true;
+    setState(() {
+      gameStarted = true;
+    });
     Timer.periodic(Duration(milliseconds: 20), (timer) {
       if (carDrivingForward == true) {
         setState(() {
           carX += 0.01;
         });
       }
-      if(carDrivingBackwards == true){
+      if (carDrivingBackwards == true) {
         setState(() {
           carX -= 0.01;
         });
@@ -37,22 +39,83 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        AnimatedContainer(
-          duration: Duration(milliseconds: 0),
-          alignment: Alignment(carX, carY),
-          child: FloatingActionButton(
-            onPressed: () {
-              carDrivingBackwards == false
-                ? carDrivingBackwards = true
-                : carDrivingBackwards = false;
-              gameStarted == false ? startGame() : null;
-            },
+    return Scaffold(
+      body: Stack(
+        children: [
+          AnimatedContainer(
+            duration: Duration(milliseconds: 0),
+            alignment: Alignment(carX, carY),
             child: UberCar(),
           ),
-        ),
-      ],
+          Positioned(
+            bottom: 0,
+            left: 0,
+            child: Container(
+              //width: 100,
+              //height: 150,
+              color: Colors.red,
+              child: Column(
+                //mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  GestureDetector(
+                    child: Icon(Icons.keyboard_arrow_up),
+                    onTapDown: (e) {
+                      carDrivingForward = true;
+                    },
+                    onTapUp: (e) {
+                      carDrivingForward = false;
+                    },
+                  ),
+                  Row(
+                    children: [
+                      IconButton(
+                          onPressed: () {
+                            carTurningLeft == false
+                                ? carTurningLeft = true
+                                : carTurningLeft = false;
+                            // ignore: unnecessary_statements
+                            gameStarted == false ? startGame() : null;
+                          },
+                          icon: Icon(Icons.keyboard_arrow_left)),
+                      IconButton(
+                          onPressed: () {
+                            carTurningRight == false
+                                ? carTurningRight = true
+                                : carTurningRight = false;
+                            // ignore: unnecessary_statements
+                            gameStarted == false ? startGame() : null;
+                          },
+                          icon: Icon(Icons.keyboard_arrow_right)),
+                    ],
+                  ),
+                  GestureDetector(
+                    child: Icon(Icons.keyboard_arrow_down),
+                    onTapDown: (e) {
+                      carDrivingBackwards = true;
+                    },
+                    onTapUp: (e) {
+                      carDrivingBackwards = false;
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Visibility(
+            visible: gameStarted == true ? false : true,
+            child: Center(
+              child: Container(
+                color: Colors.purple,
+                child: TextButton(
+                  onPressed: startGame,
+                  child: Text("START GAME",style: TextStyle(fontSize: 80,color: Colors.green),),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
